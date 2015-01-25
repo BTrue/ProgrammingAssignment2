ae20d386f53287d27899290f6fad7463c3edc018
GratefulGeek <brian.true@hotmail.com>
2015-01-24 21:03:33 -0600

    "First commit for ass2 R programming"

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Insert Comments Here when you unsderstand the function
+makeVector <- function(x = numeric()) {
+     m <- NULL
+     set <- function(y) {
+          x <<- y
+          m <<- NULL
+     }
+     get <- function() x
+     setmean <- function(mean) m <<- mean
+     getmean <- function() m
+     list(set = set, get = get,
+          setmean = setmean,
+          getmean = getmean)
+}
 
+## Insert Comments ...
+cachemean <- function(x, ...) {
+     m <- x$getmean()
+     if(!is.null(m)) {
+          message("getting cached data")
+          return(m)
+     }
+     data <- x$get()
+     m <- mean(data, ...)
+     x$setmean(m)
+     m
+}
+
+## Write a short comment describing this function
 makeCacheMatrix <- function(x = matrix()) {
+     i <- NULL
+     set <- function(y) {
+          x <<- y
+          i <<- NULL
+     }
+     get <- function() x
+     setinv <- function(mean) i <<- inv
+     getinv <- function() i
+     list(set = set, get = get,
+          setinv = setinv,
+          getinv = getinv)
 
 }
 
-
 ## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) { 
+     i <- x$getinv()
+     if(!is.null(i)) {
+          message("getting cached data")
+          return(i)
+     }
+     ## Return a matrix that is the inverse of 'x'
+     data <- x$get()
+     i <- inv(data, ...)
+     x$setinv(i)
+     i
+     
 }
